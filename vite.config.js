f7c7939da428f362ca32b17f360d6fe487de7c87
NicolasLopezSTC <gclan149@gmail.com>
2023-10-07 03:15:42 -0500

    pages?
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/public-apis/',
+  base: '/vue-restapi-magic/',
   plugins: [vue()],
   resolve: {
     alias: {
