9f201bbeca3ef7df975fc5b00d1a663846700b28
NicolasLopezSTC <gclan149@gmail.com>
2023-10-07 02:55:19 -0500

    fetched cards
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,9 +1,14 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios'
 
+
+
+
 const instance = axios.create({
   baseURL: 'https://api.magicthegathering.io/v1/',
 })
+
+
 
 const useApi = () => {
   return { instance }
